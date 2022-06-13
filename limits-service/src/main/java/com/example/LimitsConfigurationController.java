179afacffe748d31b7b68e875d1fe710c805dadc
Dejan Kulusic <dejan_kula@yahoo.com>
2022-06-13 19:15:58 +0200

    Finish

--- limits-service/src/main/java/com/example/LimitsConfigurationController.java
+++ limits-service/src/main/java/com/example/LimitsConfigurationController.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.bean.LimitsConfiguration;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 
 @RestController
 public class LimitsConfigurationController {
@@ -17,5 +18,15 @@ public class LimitsConfigurationController {
 		return new LimitsConfiguration(configuration.getMaximum(), configuration.getMinium());
 		
 	}
+	
+	  @GetMapping("/fault-tolerance-example")
+	  @HystrixCommand(fallbackMethod="fallbackRetrieveConfiguration")
+	  public LimitsConfiguration retrieveConfiguration() {
+	    throw new RuntimeException("Not available");
+	  }
+
+	  public LimitsConfiguration fallbackRetrieveConfiguration() {
+	    return new LimitsConfiguration(999, 9);
+	  }
 
 }
